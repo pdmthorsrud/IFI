303f1bd483098193a05cd5e3860a9957bbf294ad
Per Daniel Melvin Thorsrud <pdthorsr@nelleblad.ifi.uio.no>
2015-09-11 14:31:55 +0200

    Ferdig med oblig

--- Nano og Robotikk/INF2220/Oblig1/Main.java
+++ Nano og Robotikk/INF2220/Oblig1/Main.java
@@ -5,19 +5,16 @@ public class Main{
 
     public static void main(String[] args) throws Exception {
 
-	BinaryTree bt = new BinaryTree();
-	File file = new File("dictionary.txt");
-	Scanner in = new Scanner(file);
-	String read;
-	while(in.hasNextLine()){
-	    read = in.nextLine();
-	    bt.insert(read);
-	}
-	removeReInsert(bt);
-
-	bt.printStatistics();
-
-	menu(bt);
+        BinaryTree bt = new BinaryTree();
+        File file = new File("dictionary.txt");
+        Scanner in = new Scanner(file);
+        String read;
+        while(in.hasNextLine()){
+            read = in.nextLine();
+            bt.insert(read);
+        }
+        removeReInsert(bt);
+        menu(bt);
 
 
     }
@@ -25,29 +22,30 @@ public class Main{
 
     private static void removeReInsert(BinaryTree bt){
 
-	System.out.println("Removing 'busybody'");
-	bt.remove("busybody", bt.root);
-	System.out.println("Done removing 'busybody'");
-	System.out.println("Inserting 'busybody' || " + bt.insert("busybody"));
 	System.out.println("Searching for 'busybody' || " + bt.search("busybody"));
+        System.out.println("Removing 'busybody'");
+        bt.remove("busybody", bt.root);
+        System.out.println("Done removing 'busybody'");
+        System.out.println("Inserting 'busybody' || " + bt.insert("busybody"));
+        System.out.println("Searching for 'busybody' || " + bt.search("busybody"));
 
     }
 
     private static void menu(BinaryTree bt) throws Exception{
-	Scanner user = new Scanner(System.in);
-	String enteredText = "";
+        Scanner user = new Scanner(System.in);
+        String enteredText = "";
 
-	while(!enteredText.toLowerCase().equals("q")){
-	    System.out.println("\nPlease enter your searchword, then RET || 'q' to exit");
-	    enteredText = user.nextLine();
-	    if(!enteredText.toLowerCase().equals("q")){
-		bt.searchForWord(enteredText);
-	    }else{
-		System.out.println("Exit");
-		bt.writeStatistics();
-		break;
-	    }
-	}
+        while(!enteredText.toLowerCase().equals("q")){
+            System.out.println("\nPlease enter your searchword, then RET || 'q' to exit");
+            enteredText = user.nextLine();
+            if(!enteredText.toLowerCase().equals("q")){
+                bt.searchForWord(enteredText);
+            }else{
+                System.out.println("Printing statistics, then exiting: \n");
+                bt.printStatistics();
+                break;
+            }
+        }
     }
 
 }
