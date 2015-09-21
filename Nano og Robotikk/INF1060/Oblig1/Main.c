dda64574794d30ba04f85814a0b921a8a886bb99
Per Daniel Melvin Thorsrud <pdthorsr@angrod.ifi.uio.no>
2015-09-21 17:14:49 +0200

    Skrevet litt til

--- Nano og Robotikk/INF1060/Oblig1/Main.c
+++ Nano og Robotikk/INF1060/Oblig1/Main.c
@@ -12,7 +12,6 @@ typedef struct node str_node;
 
 FILE *fr;
 str_node *first = NULL;
-str_node *curr;
 
 void readFile();
 void print();
@@ -27,36 +26,44 @@ void readFile(){
   fr = fopen("tresmaa.txt", "rt");
   char text[200];
   str_node *tmp;
+  str_node *curr = first;
 
   while((fgets(text, 200, fr))!=NULL){
-    if(first==NULL){
-      printf("reading line\n");
-      first = curr;
+    if(curr==NULL){
+      printf("Reading first line\n");
       curr = malloc(sizeof(str_node));
       curr->data = malloc(strlen(text)+1);
       curr->data = text;
+      first = curr;
+      printf("Printing text: %s", text);
+      printf("Printing curr->data: %s\n", first->data);
     }else{
-      printf("reading line\n");
+      printf("Reading rest of the lines\n");
       tmp = curr;
       tmp->next = curr;
       curr = malloc(sizeof(str_node));
       curr->data=malloc(strlen(text)+1);
       curr->data = text;
+      printf("Printing text: %s", text);
+      printf("Printing curr->data: %s\n", curr->data);
     }
+  }
+  if(curr==NULL){
+    printf("Curr er null her ogsaa");
   }
   fclose(fr);
 }
 
 void print(){
+  printf("Hello, nr. 2");
+  str_node *curr = first;
 
-  curr = first;
-
-  while(curr!=NULL){
-    printf("Printing line\n");
-    printf("%s\n",curr->data);
-    curr = curr->next;
-    printf("Finished printing line\n");
-  }
+  printf("Printing first->data: %s\n", first->data);
+  
+  //while(curr!=NULL){
+  //printf("%s\n",curr->data);
+  //curr = curr->next;
+  //}
   
 
 }
