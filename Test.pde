e723ce2aa84018fe64396792de72f7f09511b9c8
pdthorsr <sagittarius@live.no>
2016-08-18 16:15:07 +0200

    Fjernet emacs dir

--- Test.pde
+++
@@ -1,8 +0,0 @@
-void setup(){
-  background(0,0,0);
-}
-
-void draw(){
-  fill(255,0,0);
-  rect(0,0,80,10);
-}
--- emacs/elisp/orgMode.el
+++
@@ -1,2 +0,0 @@
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(define-key global-map "\C-cc" 'org-capture)
